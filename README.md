44e845a001164fe1626ea36ec62d5c1f775e030b
Rubin Ghimire <rubin.ghimire@gmail.com>
2023-10-02 13:43:33 +0545

    project files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python script to extract headings from Powerpoint file
+
+### Prerequisites
+* pip install python-pptx
+* Copy your slide to files folder
+* Change the path in main.py
+
+### Note
+* Can be easily modified to extract the body text of each slide
---
+++ main.py
@@ -0,0 +1,39 @@
+from pptx import Presentation
+
+def extract_slide_headings(pptx_file_path):
+    # Load the PowerPoint presentation
+    presentation = Presentation(pptx_file_path)
+
+    # Initialize a list to store the extracted headings
+    headings = []
+
+    # Iterate through slides and extract headings from slide layouts
+    for slide in presentation.slides:
+        if slide.shapes.title is not None:
+            heading = slide.shapes.title.text
+
+            # Replace non-breaking space (nbsp) and vertical tab (vt) with regular spaces
+            heading = heading.replace('\xa0', ' ').replace('\x0b', ' ')
+
+            if heading.strip():  # Check if the heading is not empty
+                headings.append(heading)
+
+    return headings
+
+
+# Replace 'your_presentation.pptx' with the path to your PowerPoint file
+pptx_file_path = '..\\files\\your_presentation.pptx'
+
+# Extract slide headings from the PowerPoint file
+headings = extract_slide_headings(pptx_file_path)
+
+# Define the output file path
+output_file = '..\\files\\extracted_headings.txt'
+
+# Write the extracted headings to a text file with a new line after each heading
+with open(output_file, 'w', encoding='utf-8') as file:
+    for i, heading in enumerate(headings, start=1):
+        file.write(f"Heading {i}: {heading}\n\n")  # Add a new line after each heading
+
+# Print a message indicating where the headings have been saved
+print(f"Extracted slide headings saved to '{output_file}'")
